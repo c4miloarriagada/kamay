7f6f7f93ee93120a956924a591c0ec3287cf8bf3
c4miloarriagada <camilo.arriagadav@gmail.com>
2024-04-16 21:47:07 -0400

    hotfix: deleted base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/',
   reactStrictMode: true,
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
